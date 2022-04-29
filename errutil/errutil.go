daa4075f1a3145cce15677ee9d174cfb7d613695
Hoang <dev+g@hvvo.dev>
2022-04-29 10:38:25 +0700

    add WrapFnError

--- errutil/errutil.go
+++ errutil/errutil.go
@@ -2,6 +2,7 @@ package errutil
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/midacode/common-go/httputil"
 	"github.com/midacode/common-go/validator"
@@ -9,4 +10,9 @@ import (
 
 func IsExpectedError(err error) bool {
 	return errors.Is(err, httputil.ErrBadRequest) || errors.Is(err, httputil.ErrUnauthorized) || errors.Is(err, httputil.ErrForbidden) || errors.Is(err, httputil.ErrNotFound) || errors.Is(err, httputil.ErrInternal) || errors.Is(err, httputil.ErrConflict) || errors.As(err, &validator.ValidationError{})
+}
+
+func WrapFnError(err *error, format string, args ...interface{}) {
+	s := fmt.Sprintf(format, args...)
+	*err = fmt.Errorf("%s: %w", s, *err)
 }
